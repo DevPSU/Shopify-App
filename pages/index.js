4440d47b7b4a8c21b133cb36e7f3fd5887644056
Gabe <glstewart17@gmail.com>
2019-04-01 02:45:06 -0400

    Edit Index and Add Images

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,108 @@
-import { EmptyState, Layout, Page , ResourcePicker} from '@shopify/polaris';
-import cookies from 'js-cookie';
-import Cookies from 'js-cookie';
-import store from 'store-js';
+import React from 'react';
+import {Card, DescriptionList, ResourceList, Layout, List, Page, TextStyle, Thumbnail} from '@shopify/polaris';
 
-class Index extends React.Component {
-state = { open: false };
-    render() {
-            return (
-            <Page
-                primaryAction={{
-                    content: 'Select products',
-                    // onAction: () => this.setState({open: true}),
-                    onAction: () => this.rewrite(),
+class classname extends React.Component {
 
-                }}
-            >
-                <ResourcePicker
-                    open={this.state.open}
-                    resourceType="Product"
-                    showVariants={false}
-                    onSelection={(resources) => this.handleSelection(resources)}
-                    onCancel={() => this.setState({open: false})}/>
-                <Layout>
-                    <EmptyState
-                        heading="Select products to start"
-                        action={{
-                            content: 'Select products',
-                            onAction: () => this.setState({open: true}),
-                        }}
-                        image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
-                    >
-                        <p>Select products and change their price temporarily</p>
-                    </EmptyState>
-                </Layout>
-            </Page>
-            );
-     }
-     handleSelection = (resources) => {
-        const idsFromResources = resources.selection.map((product) => product.id);
-        this.setState({open: false});
-         console.log(resources)
-     }
+render() {
+    return (
+      <Page>
+          <h1 class="Polaris-DisplayText Polaris-DisplayText--sizeLarge">Shopify Store Analytics</h1>
+          <p><br/></p>
+          <div> </div>
+          <Layout>
+            <Layout.Section>
+            <Card>
+                  <Card.Section>
+                    <h2 class="Polaris-Heading">Project Overview</h2>
+                  </Card.Section>
+                  <Card.Section>
+                    <List type="bullet">
+                      <List.Item>Determine the most popular products for each purchase</List.Item>
+                      <List.Item>Calculate customer churn</List.Item>
+                      <List.Item>Find average time between purchases</List.Item>
+                    </List>
+                  </Card.Section>
+            </Card>
+            <Card>
+                  <Card.Section>
+                    <h2 class="Polaris-Heading">Our Sponsors</h2>
+                  </Card.Section>
+                  <Card.Section>
+                    <ResourceList
+                      resourceName={{singular: 'customer', plural: 'customers'}}
+                      items={[
+                        {
+                          id: 341,
+                          url: "https://acm.psu.edu/wp-content/uploads/2018/10/DEVPSULOGO-1024x1024.jpg",
+                          name: "DevPSU Logo",
+                          text: 'Developed with DevPSU',
+                        },
+                        {
+                          id: 256,
+                          url: "http://static1.squarespace.com/static/5c63721a34c4e223bbcd826d/t/5c6726c1e79c704ba4cab911/1550264001810/wavebreak-logo.png?format=1500w",
+                          name: "Wavebreak Logo",
+                          text: 'Tasked by Wavebreak',
+                        },
+                      ]}
+                      renderItem={(item) => {
+                        const {id, url, name, text} = item;
 
-     async rewrite(){
-        console.log("rewrite");
-        console.log(cookies.get('accessToken'));
-         const options = {
-             method: 'GET',
-             credentials: 'include',
-             headers: {
-                 'X-Shopify-Access-Token': cookies.get('accessToken'),
-                 'Content-Type': 'application/json',
-             },
-         };
-         await fetch(`https://${cookies.get('shopOrigin')}/admin/orders.json?status=any`, options)
-             .then((response) => response.json())
-             .then((data) => {
-                 cookies.set
-                 console.log(data);
-             })
-             .catch((error) => console.error( error));
-     }
+                        return (
+                            <ResourceList.Item
+                              id={id}
+                              media={(
+                                <Thumbnail
+                                  source={url}
+                                  size="large"
+                                  alt={name}
+                                />
+                              )}
+                              accessibilityLabel={`View details for ${name}`}
+                            >
+                              <h3>
+                                <TextStyle variation="strong">{text}</TextStyle>
+                              </h3>
+                            </ResourceList.Item>
+                        );
+                      }}
+                    />
+                  </Card.Section>
+                </Card>
+                <Card>
+                  <Card.Section>
+                    <h2 class="Polaris-Heading">Our Team</h2>
+                  </Card.Section>
+                  <Card.Section>
+                    <DescriptionList
+                      items={[
+                        {
+                          term: 'Gabriel Stewart',
+                          description: 'Software Engineer',},
+                        {
+                          term: 'Joshua Yan',
+                          description: 'Software Engineer',
+                        },
+                        {
+                          term: 'Stanley Kwok ',
+                          description: 'Software Engineer',
+                        },
+                        {
+                          term: 'Louden Maclay',
+                          description: 'Software Engineer',
+                        },
+                        {
+                          term: 'Robert Hsu',
+                          description: 'Project Manager',
+                        },
+                      ]}
+                    />
+                  </Card.Section>
+                </Card>
+            </Layout.Section>
+          </Layout>
+        </Page>
+    );
+  }
 }
 
-export default Index;
+export default classname;
